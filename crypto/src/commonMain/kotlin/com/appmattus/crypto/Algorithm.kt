133f0537c88e70337a5c23fd72d996264fb94e05
Matthew Dolan <matt.dolan@babylonhealth.com>
2021-03-09 15:56:02 +0000

    Update Algorithm to a sealed class

--- crypto/src/commonMain/kotlin/com/appmattus/crypto/Algorithm.kt
+++ crypto/src/commonMain/kotlin/com/appmattus/crypto/Algorithm.kt
@@ -16,155 +16,155 @@
 
 package com.appmattus.crypto
 
-@Suppress("EnumEntryName", "MagicNumber")
-enum class Algorithm(val algorithmName: String, internal val blockLength: Int) {
-    Adler32("Adler32", 32),
+@Suppress("MagicNumber", "ClassName")
+sealed class Algorithm(val algorithmName: String, internal val blockLength: Int) {
+    object Adler32 : Algorithm("Adler32", 32)
 
-    BLAKE224("BLAKE-224", 64),
-    BLAKE256("BLAKE-256", 64),
-    BLAKE384("BLAKE-384", 128),
-    BLAKE512("BLAKE-512", 128),
+    object BLAKE224 : Algorithm("BLAKE-224", 64)
+    object BLAKE256 : Algorithm("BLAKE-256", 64)
+    object BLAKE384 : Algorithm("BLAKE-384", 128)
+    object BLAKE512 : Algorithm("BLAKE-512", 128)
 
-    BMW224("BMW-224", 64),
-    BMW256("BMW-256", 64),
-    BMW384("BMW-384", 128),
-    BMW512("BMW-512", 128),
+    object BMW224 : Algorithm("BMW-224", 64)
+    object BMW256 : Algorithm("BMW-256", 64)
+    object BMW384 : Algorithm("BMW-384", 128)
+    object BMW512 : Algorithm("BMW-512", 128)
 
-    CRC32("CRC32", 32),
+    object CRC32 : Algorithm("CRC32", 32)
 
-    CubeHash224("CubeHash-224", 32),
-    CubeHash256("CubeHash-256", 32),
-    CubeHash384("CubeHash-384", 32),
-    CubeHash512("CubeHash-512", 32),
+    object CubeHash224 : Algorithm("CubeHash-224", 32)
+    object CubeHash256 : Algorithm("CubeHash-256", 32)
+    object CubeHash384 : Algorithm("CubeHash-384", 32)
+    object CubeHash512 : Algorithm("CubeHash-512", 32)
 
-    DSTU7564_256("DSTU7564-256", 64),
-    DSTU7564_384("DSTU7564-384", 128),
-    DSTU7564_512("DSTU7564-512", 128),
+    object DSTU7564_256 : Algorithm("DSTU7564-256", 64)
+    object DSTU7564_384 : Algorithm("DSTU7564-384", 128)
+    object DSTU7564_512 : Algorithm("DSTU7564-512", 128)
 
-    ECHO224("ECHO-224", 192),
-    ECHO256("ECHO-256", 192),
-    ECHO384("ECHO-384", 128),
-    ECHO512("ECHO-512", 128),
+    object ECHO224 : Algorithm("ECHO-224", 192)
+    object ECHO256 : Algorithm("ECHO-256", 192)
+    object ECHO384 : Algorithm("ECHO-384", 128)
+    object ECHO512 : Algorithm("ECHO-512", 128)
 
-    Fugue224("Fugue-224", 28),
-    Fugue256("Fugue-256", 32),
-    Fugue384("Fugue-384", 48),
-    Fugue512("Fugue-512", 64),
+    object Fugue224 : Algorithm("Fugue-224", 28)
+    object Fugue256 : Algorithm("Fugue-256", 32)
+    object Fugue384 : Algorithm("Fugue-384", 48)
+    object Fugue512 : Algorithm("Fugue-512", 64)
 
-    GOST3411_94("GOST3411", 32),
-    GOST3411_2012_256("GOST3411-2012-256", 64),
-    GOST3411_2012_512("GOST3411-2012-512", 64),
+    object GOST3411_94 : Algorithm("GOST3411", 32)
+    object GOST3411_2012_256 : Algorithm("GOST3411-2012-256", 64)
+    object GOST3411_2012_512 : Algorithm("GOST3411-2012-512", 64)
 
-    Groestl224("Groestl-224", 64),
-    Groestl256("Groestl-256", 64),
-    Groestl384("Groestl-384", 128),
-    Groestl512("Groestl-512", 128),
+    object Groestl224 : Algorithm("Groestl-224", 64)
+    object Groestl256 : Algorithm("Groestl-256", 64)
+    object Groestl384 : Algorithm("Groestl-384", 128)
+    object Groestl512 : Algorithm("Groestl-512", 128)
 
-    Hamsi224("Hamsi-224", 32),
-    Hamsi256("Hamsi-256", 32),
-    Hamsi384("Hamsi-384", 32),
-    Hamsi512("Hamsi-512", 32),
+    object Hamsi224 : Algorithm("Hamsi-224", 32)
+    object Hamsi256 : Algorithm("Hamsi-256", 32)
+    object Hamsi384 : Algorithm("Hamsi-384", 32)
+    object Hamsi512 : Algorithm("Hamsi-512", 32)
 
-    Haraka256_256("Haraka-256", 32),
-    Haraka512_256("Haraka-512", 64),
+    object Haraka256_256 : Algorithm("Haraka-256", 32)
+    object Haraka512_256 : Algorithm("Haraka-512", 64)
 
-    HAVAL_3_128("HAVAL-3-128", 128),
-    HAVAL_3_160("HAVAL-3-160", 128),
-    HAVAL_3_192("HAVAL-3-192", 128),
-    HAVAL_3_224("HAVAL-3-224", 128),
-    HAVAL_3_256("HAVAL-3-256", 128),
-    HAVAL_4_128("HAVAL-4-128", 128),
-    HAVAL_4_160("HAVAL-4-160", 128),
-    HAVAL_4_192("HAVAL-4-192", 128),
-    HAVAL_4_224("HAVAL-4-224", 128),
-    HAVAL_4_256("HAVAL-4-256", 128),
-    HAVAL_5_128("HAVAL-5-128", 128),
-    HAVAL_5_160("HAVAL-5-160", 128),
-    HAVAL_5_192("HAVAL-5-192", 128),
-    HAVAL_5_224("HAVAL-5-224", 128),
-    HAVAL_5_256("HAVAL-5-256", 128),
+    object HAVAL_3_128 : Algorithm("HAVAL-3-128", 128)
+    object HAVAL_3_160 : Algorithm("HAVAL-3-160", 128)
+    object HAVAL_3_192 : Algorithm("HAVAL-3-192", 128)
+    object HAVAL_3_224 : Algorithm("HAVAL-3-224", 128)
+    object HAVAL_3_256 : Algorithm("HAVAL-3-256", 128)
+    object HAVAL_4_128 : Algorithm("HAVAL-4-128", 128)
+    object HAVAL_4_160 : Algorithm("HAVAL-4-160", 128)
+    object HAVAL_4_192 : Algorithm("HAVAL-4-192", 128)
+    object HAVAL_4_224 : Algorithm("HAVAL-4-224", 128)
+    object HAVAL_4_256 : Algorithm("HAVAL-4-256", 128)
+    object HAVAL_5_128 : Algorithm("HAVAL-5-128", 128)
+    object HAVAL_5_160 : Algorithm("HAVAL-5-160", 128)
+    object HAVAL_5_192 : Algorithm("HAVAL-5-192", 128)
+    object HAVAL_5_224 : Algorithm("HAVAL-5-224", 128)
+    object HAVAL_5_256 : Algorithm("HAVAL-5-256", 128)
 
-    JH224("JH-224", 64),
-    JH256("JH-256", 64),
-    JH384("JH-384", 64),
-    JH512("JH-512", 64),
+    object JH224 : Algorithm("JH-224", 64)
+    object JH256 : Algorithm("JH-256", 64)
+    object JH384 : Algorithm("JH-384", 64)
+    object JH512 : Algorithm("JH-512", 64)
 
-    Keccak224("Keccak-224", 144),
-    Keccak256("Keccak-256", 136),
-    Keccak288("Keccak-288", 128),
-    Keccak384("Keccak-384", 104),
-    Keccak512("Keccak-512", 72),
+    object Keccak224 : Algorithm("Keccak-224", 64)
+    object Keccak256 : Algorithm("Keccak-256", 64)
+    object Keccak288 : Algorithm("Keccak-288", 64)
+    object Keccak384 : Algorithm("Keccak-384", 128)
+    object Keccak512 : Algorithm("Keccak-512", 128)
 
-    Luffa224("Luffa-224", 32),
-    Luffa256("Luffa-256", 32),
-    Luffa384("Luffa-384", 32),
-    Luffa512("Luffa-512", 32),
+    object Luffa224 : Algorithm("Luffa-224", 32)
+    object Luffa256 : Algorithm("Luffa-256", 32)
+    object Luffa384 : Algorithm("Luffa-384", 32)
+    object Luffa512 : Algorithm("Luffa-512", 32)
 
-    MD2("MD2", 16),
-    MD4("MD4", 64),
-    MD5("MD5", 64),
+    object MD2 : Algorithm("MD2", 16)
+    object MD4 : Algorithm("MD4", 64)
+    object MD5 : Algorithm("MD5", 64)
 
-    PANAMA("PANAMA", 32),
+    object PANAMA : Algorithm("PANAMA", 32)
 
-    RadioGatun32("RadioGatún[32]", 156),
-    RadioGatun64("RadioGatún[64]", 312),
+    object RadioGatun32 : Algorithm("RadioGatún[32]", 156)
+    object RadioGatun64 : Algorithm("RadioGatún[64]", 312)
 
-    RipeMD("RipeMD", 64),
-    RipeMD128("RipeMD128", 64),
-    RipeMD160("RipeMD160", 64),
-    RipeMD256("RipeMD256", 64),
-    RipeMD320("RipeMD320", 64),
+    object RipeMD : Algorithm("RipeMD", 64)
+    object RipeMD128 : Algorithm("RipeMD128", 64)
+    object RipeMD160 : Algorithm("RipeMD160", 64)
+    object RipeMD256 : Algorithm("RipeMD256", 64)
+    object RipeMD320 : Algorithm("RipeMD320", 64)
 
-    SHA_0("SHA-0", 64),
-    SHA_1("SHA-1", 64),
-    SHA_224("SHA-224", 64),
-    SHA_256("SHA-256", 64),
-    SHA_384("SHA-384", 128),
-    SHA_512("SHA-512", 128),
-    SHA_512_224("SHA-512/224", 128),
-    SHA_512_256("SHA-512/256", 128),
+    object SHA_0 : Algorithm("SHA-0", 64)
+    object SHA_1 : Algorithm("SHA-1", 64)
+    object SHA_224 : Algorithm("SHA-224", 64)
+    object SHA_256 : Algorithm("SHA-256", 64)
+    object SHA_384 : Algorithm("SHA-384", 128)
+    object SHA_512 : Algorithm("SHA-512", 128)
+    object SHA_512_224 : Algorithm("SHA-512/224", 128)
+    object SHA_512_256 : Algorithm("SHA-512/256", 128)
 
-    SHA3_224("SHA3-224", 144),
-    SHA3_256("SHA3-256", 136),
-    SHA3_384("SHA3-384", 104),
-    SHA3_512("SHA3-512", 72),
+    object SHA3_224 : Algorithm("SHA3-224", 64)
+    object SHA3_256 : Algorithm("SHA3-256", 64)
+    object SHA3_384 : Algorithm("SHA3-384", 128)
+    object SHA3_512 : Algorithm("SHA3-512", 128)
 
-    Shabal192("Shabal-192", 64),
-    Shabal224("Shabal-224", 64),
-    Shabal256("Shabal-256", 64),
-    Shabal384("Shabal-384", 64),
-    Shabal512("Shabal-512", 64),
+    object Shabal192 : Algorithm("Shabal-192", 64)
+    object Shabal224 : Algorithm("Shabal-224", 64)
+    object Shabal256 : Algorithm("Shabal-256", 64)
+    object Shabal384 : Algorithm("Shabal-384", 64)
+    object Shabal512 : Algorithm("Shabal-512", 64)
 
-    SHAvite224("SHAvite-224", 64),
-    SHAvite256("SHAvite-256", 64),
-    SHAvite384("SHAvite-384", 128),
-    SHAvite512("SHAvite-512", 128),
+    object SHAvite224 : Algorithm("SHAvite-224", 64)
+    object SHAvite256 : Algorithm("SHAvite-256", 64)
+    object SHAvite384 : Algorithm("SHAvite-384", 128)
+    object SHAvite512 : Algorithm("SHAvite-512", 128)
 
-    SIMD224("SIMD-224", 64),
-    SIMD256("SIMD-256", 64),
-    SIMD384("SIMD-384", 128),
-    SIMD512("SIMD-512", 128),
+    object SIMD224 : Algorithm("SIMD-224", 64)
+    object SIMD256 : Algorithm("SIMD-256", 64)
+    object SIMD384 : Algorithm("SIMD-384", 128)
+    object SIMD512 : Algorithm("SIMD-512", 128)
 
-    Skein256_128("Skein-256-128", 32),
-    Skein256_160("Skein-256-160", 32),
-    Skein256_224("Skein-256-224", 32),
-    Skein256_256("Skein-256-256", 32),
-    Skein512_128("Skein-512-128", 64),
-    Skein512_160("Skein-512-160", 64),
-    Skein512_224("Skein-512-224", 64),
-    Skein512_256("Skein-512-256", 64),
-    Skein512_384("Skein-512-384", 64),
-    Skein512_512("Skein-512-512", 64),
-    Skein1024_384("Skein-1024-384", 128),
-    Skein1024_512("Skein-1024-512", 128),
-    Skein1024_1024("Skein-1024-1024", 128),
+    object Skein256_128 : Algorithm("Skein-256-128", 32)
+    object Skein256_160 : Algorithm("Skein-256-160", 32)
+    object Skein256_224 : Algorithm("Skein-256-224", 32)
+    object Skein256_256 : Algorithm("Skein-256-256", 32)
+    object Skein512_128 : Algorithm("Skein-512-128", 64)
+    object Skein512_160 : Algorithm("Skein-512-160", 64)
+    object Skein512_224 : Algorithm("Skein-512-224", 64)
+    object Skein512_256 : Algorithm("Skein-512-256", 64)
+    object Skein512_384 : Algorithm("Skein-512-384", 64)
+    object Skein512_512 : Algorithm("Skein-512-512", 64)
+    object Skein1024_384 : Algorithm("Skein-1024-384", 128)
+    object Skein1024_512 : Algorithm("Skein-1024-512", 128)
+    object Skein1024_1024 : Algorithm("Skein-1024-1024", 128)
 
-    SM3("SM3", 64),
+    object SM3 : Algorithm("SM3", 64)
 
-    Tiger("Tiger", 64),
-    Tiger2("Tiger2", 64),
+    object Tiger : Algorithm("Tiger", 64)
+    object Tiger2 : Algorithm("Tiger2", 64)
 
-    Whirlpool("Whirlpool", 64),
-    Whirlpool0("Whirlpool-0", 64),
-    WhirlpoolT("Whirlpool-T", 64),
+    object Whirlpool : Algorithm("Whirlpool", 64)
+    object Whirlpool0 : Algorithm("Whirlpool-0", 64)
+    object WhirlpoolT : Algorithm("Whirlpool-T", 64)
 }
